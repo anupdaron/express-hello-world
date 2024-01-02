1a8ec13db71cec4d6300b1726d21ea90ca4051c0
anupdaron <55269672+anupdaron@users.noreply.github.com>
2024-01-02 19:42:54 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const path = require("path");
 const app = express()
+const fs = require('fs'); 
+// Specify a directory to serve files from
+const publicDirectoryPath = path.join(__dirname, '/public');
+app.use(express.static(publicDirectoryPath));
+app.use(express.json({ limit: '100000mb' }));
+// Create a directory if it doesn't exist
+if (!fs.existsSync(publicDirectoryPath)){
+    fs.mkdirSync(publicDirectoryPath);
+}
 
 // #############################################################################
 // Logs all request paths and method
@@ -9,6 +18,29 @@ app.use(function (req, res, next) {
   res.set('x-powered-by', 'cyclic.sh')
   console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
   next();
+});
+// create a route for the app to create jsonl file with received data
+app.post('/create', (req, res) => {
+  // Generate a unique filename
+  const uniqueFilename = `finetune_${Date.now()}_${Math.floor(Math.random() * 1000)}.jsonl`;
+  const filePath = path.join(publicDirectoryPath, uniqueFilename);
+
+  // Create jsonl file with received data
+  const data = req.body;
+  console.log(data)
+  const jsonl = data.map((item) => JSON.stringify(item)).join('\n');
+
+  fs.writeFile(filePath, jsonl, (err) => {
+    if (err) {
+      console.error('Error saving file:', err);
+      res.status(500).send('Error saving file');
+    } else {
+      console.log('The file has been saved!');
+      // Return the download URL
+      const downloadUrl = `http://${req.headers.host}/${uniqueFilename}`;
+      res.send({ downloadUrl });
+    }
+  });
 });
 
 // #############################################################################
